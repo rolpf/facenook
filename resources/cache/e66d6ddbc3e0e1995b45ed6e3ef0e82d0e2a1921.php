371c993ef556ff52aef563a431c560b822eef991
rolpf <jiroxane@hotmail.fr>
2021-12-02 17:22:32 +0100

    cache a mettre dans gitignore

--- resources/cache/e66d6ddbc3e0e1995b45ed6e3ef0e82d0e2a1921.php
+++ resources/cache/e66d6ddbc3e0e1995b45ed6e3ef0e82d0e2a1921.php
@@ -10,10 +10,6 @@
         <input type="password" id="input_password" name="mdp" placeholder="Mot de passe" required>
         <input type="submit" id="input_submit"name="valider" value="Inscription">
 </form>
-
-</div>
-=======
-
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
