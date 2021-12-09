67c9db27c9465fcf039b7c58f46be018a9c2b835
rolpf <jiroxane@hotmail.fr>
2021-12-09 10:55:35 +0100

    correction menu gauche

--- resources/cache/0b4c7309a822594da56ac121a5a661ae61f4c531.php
+++ resources/cache/0b4c7309a822594da56ac121a5a661ae61f4c531.php
@@ -1,18 +1,10 @@
 <?php $__env->startSection('content'); ?>
     <section class="container-profil">
-        <div class="menu-gauche">
-            <h3></h3>
-
-        </div>
 
         <div class="profil">
         <h1 class="title-page">Demandes d'amis</h1>
         </div>
 
-        <div class="menu-droite">
-            
-
-        </div>
 
 
 
--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -2,19 +2,11 @@
 
 @section('content')
     <section class="container-profil">
-        <div class="menu-gauche">
-            <h3></h3>
-
-        </div>
 
         <div class="profil">
         <h1 class="title-page">Demandes d'amis</h1>
         </div>
 
-        <div class="menu-droite">
-            
-
-        </div>
 
 
 
